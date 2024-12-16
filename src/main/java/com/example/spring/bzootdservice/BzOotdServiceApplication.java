4ddef12959a12a2cf94cf29a022ff3e757b921f4
Ninky0 <2020113392@dgu.ac.kr>
2024-12-16 16:07:58 +0900

    init:setting

---
+++ src/main/java/com/example/spring/bzootdservice/BzOotdServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.bzootdservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzOotdServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BzOotdServiceApplication.class, args);
+    }
+
+}
